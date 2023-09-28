08e23039553ac4ce66162231509fa8b9ca3ac231
Andre Neves <andre@zebedee.io>
2023-09-28 10:03:19 -0700

    chore: types

--- src/index.ts
+++ src/index.ts
@@ -14,115 +14,157 @@ export const getZBDProvider = ({
   clientSecret: string | any;
   apiKey: string | any;
   scope: string | any;
-}) => ({
-  id: "zbd",
-  name: "ZBD",
-  type: "oauth",
-  clientId,
-  clientSecret,
-  authorization: {
-    url: ZBD_AUTH_URL,
-    params: { scope: scope }
-  },
-  token: ZBD_TOKEN_URL,
-  checks: ["pkce", "state"],
-  userinfo: {
-    async request(context: any) {
-      // Default empty user wallet obj
-      const emptyUserWallet = {
-        "balance": null,
-        "remainingAmountLimits": {
-          "daily": null,
-          "maxCredit": null,
-          "monthly": null,
-          "weekly": null,
-        }
-      }
-
-      // Default empty user profile obj
-      const emptyUserProfile = {
-        "id": null,
-        "email": null,
-        "gamertag": null,
-        "image": null,
-        "isVerified": null,
-        "lightningAddress": null,
-        "publicBio": null,
-        "publicStaticCharge": null,
-        "social": {},
-      }
-
-      const headers: { apikey: string; usertoken?: string; } = {
-        apikey: apiKey,
+}) => {
+  const provider: {
+    id: any;
+    name: any;
+    type: any;
+    clientId: any;
+    clientSecret: any;
+    authorization: {
+      url: any;
+      params: {
+        scope: any;
       };
-
-      if (context.tokens.access_token) {
-        headers['usertoken'] = context.tokens.access_token;
-      };
-
-      if (!headers['usertoken']) {
-        console.log('No access_token was found from the response of authorization request.');
-        return { ...emptyUserProfile, ...emptyUserWallet };
-      };
-
-      // Fetching user profile and wallet data in parallel
-      const t = await fetch(ZBD_USER_DATA_URL, {
-        headers,
-      });
-      const d = await fetch(ZBD_WALLET_DATA_URL, {
-        headers,
-      });
-      
-      // Handling response from both fetches
-      let all = {}
-      if (t.ok) {
-        const p = await t.json();
-        all = { ...p.data };
-      } else {
-        console.log(`Response from ${ZBD_USER_DATA_URL} resulted in the following:`);
-        console.log('Status Code:', t.status);
-        console.log('Response Text:', t.statusText);
-        all = { ...emptyUserProfile };
-      };
-
-      if (d.ok) {
-        const p = await d.json();
-        all = { ...all, ...p.data };
-      } else {
-        console.log(`Response from ${ZBD_WALLET_DATA_URL} resulted in the following:`);
-        console.log('Status Code:', t.status);
-        console.log('Response Text:', t.statusText);
-        all = { ...all, ...emptyUserWallet };
-      };
-
-      // Returning all data
-      return all;
+    };
+    token: any;
+    checks: any;
+    userinfo: {
+      request(context: any): Promise<{}>;
+    };
+    profile(profile: any): {
+      id: any;
+      email: any;
+      gamertag: any;
+      image: any;
+      isVerified: any;
+      lightningAddress: any;
+      publicBio: any;
+      publicStaticCharge: any;
+      social: any;
+      balance: any;
+      remainingAmountLimits: any;
+    };
+    style: {
+      logo: string;
+      logoDark: string;
+      bg: string;
+      text: string;
+      bgDark: string;
+      textDark: string;
+    };
+  } = {
+    id: "zbd",
+    name: "ZBD",
+    type: "oauth",
+    clientId,
+    clientSecret,
+    authorization: {
+      url: ZBD_AUTH_URL,
+      params: { scope: scope }
     },
-  },
-  profile(profile: any) {
-    return {
-      // User profile properties
-      "id": profile['id'],
-      "email": profile['email'],
-      "gamertag": profile['gamertag'],
-      "image": profile['image'],
-      "isVerified": profile['isVerified'],
-      "lightningAddress": profile['lightningAddress'],
-      "publicBio": profile['publicBio'],
-      "publicStaticCharge": profile['publicStaticCharge'],
-      "social": profile['social'],
+    token: ZBD_TOKEN_URL,
+    checks: ["pkce", "state"],
+    userinfo: {
+      async request(context: any) {
+        // Default empty user wallet obj
+        const emptyUserWallet = {
+          "balance": null,
+          "remainingAmountLimits": {
+            "daily": null,
+            "maxCredit": null,
+            "monthly": null,
+            "weekly": null,
+          }
+        }
 
-      // User wallet properties
-      "balance": profile['balance'],
-      "remainingAmountLimits": profile['remainingAmountLimits'],
-    }
-  },
-  style: {
-    logo: "https://cdn.zebedee.io/zbdgg/social/zbd-pfp-default.png",
-    logoDark: "https://cdn.zebedee.io/zbdgg/social/zbd-pfp-default.png",
-    bg: "#fff",
-    text: "#000",
-    bgDark: "#000",
-    textDark: "#fff",
-  },
-});
+        // Default empty user profile obj
+        const emptyUserProfile = {
+          "id": null,
+          "email": null,
+          "gamertag": null,
+          "image": null,
+          "isVerified": null,
+          "lightningAddress": null,
+          "publicBio": null,
+          "publicStaticCharge": null,
+          "social": {},
+        }
+
+        const headers: { apikey: string; usertoken?: string; } = {
+          apikey: apiKey,
+        };
+
+        if (context.tokens.access_token) {
+          headers['usertoken'] = context.tokens.access_token;
+        };
+
+        if (!headers['usertoken']) {
+          console.log('No access_token was found from the response of authorization request.');
+          return { ...emptyUserProfile, ...emptyUserWallet };
+        };
+
+        // Fetching user profile and wallet data in parallel
+        const t = await fetch(ZBD_USER_DATA_URL, {
+          headers,
+        });
+        const d = await fetch(ZBD_WALLET_DATA_URL, {
+          headers,
+        });
+        
+        // Handling response from both fetches
+        let all = {}
+        if (t.ok) {
+          const p = await t.json();
+          all = { ...p.data };
+        } else {
+          console.log(`Response from ${ZBD_USER_DATA_URL} resulted in the following:`);
+          console.log('Status Code:', t.status);
+          console.log('Response Text:', t.statusText);
+          all = { ...emptyUserProfile };
+        };
+
+        if (d.ok) {
+          const p = await d.json();
+          all = { ...all, ...p.data };
+        } else {
+          console.log(`Response from ${ZBD_WALLET_DATA_URL} resulted in the following:`);
+          console.log('Status Code:', t.status);
+          console.log('Response Text:', t.statusText);
+          all = { ...all, ...emptyUserWallet };
+        };
+
+        // Returning all data
+        return all;
+      },
+    },
+    profile(profile: any) {
+      return {
+        // User profile properties
+        "id": profile['id'],
+        "email": profile['email'],
+        "gamertag": profile['gamertag'],
+        "image": profile['image'],
+        "isVerified": profile['isVerified'],
+        "lightningAddress": profile['lightningAddress'],
+        "publicBio": profile['publicBio'],
+        "publicStaticCharge": profile['publicStaticCharge'],
+        "social": profile['social'],
+
+        // User wallet properties
+        "balance": profile['balance'],
+        "remainingAmountLimits": profile['remainingAmountLimits'],
+      }
+    },
+    style: {
+      logo: "https://cdn.zebedee.io/zbdgg/social/zbd-pfp-default.png",
+      logoDark: "https://cdn.zebedee.io/zbdgg/social/zbd-pfp-default.png",
+      bg: "#fff",
+      text: "#000",
+      bgDark: "#000",
+      textDark: "#fff",
+    },
+  };
+
+  return provider;
+};
